4de925ff3dcaf388e76c58d593785bca0002421b
paul-431 <132036510+paul-431@users.noreply.github.com>
2023-12-09 01:04:23 +0100

    Add files via upload
    creation d'un crud

---
+++ create_table.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
+            $servname = 'localhost';
+            $dbname = 'pdodb';
+            $user = 'root';
+            $pass = '';
+            
+            try{
+                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "CREATE TABLE etudiant(
+                        Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        first_name VARCHAR(30) NOT NULL,
+                        last_name VARCHAR(30) NOT NULL,
+                        Adresse VARCHAR(70) NOT NULL,
+                        gender VARCHAR(30) NOT NULL,
+                        age INT UNSIGNED NOT NULL,
+                        email VARCHAR(50) NOT NULL,
+                        UNIQUE(email))";
+            
+                $dbco->exec($sql);
+                echo 'Table bien créée !';
+            }
+            
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+        ?>
+    </body>
+</html>
